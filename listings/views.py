55de890004e005d93ccbf5cea066548d00aaa037
Steve Kamanke <steve.kamanke@double-eye.com>
2019-08-10 19:31:01 +0200

    dynamic template updates

--- listings/views.py
+++ listings/views.py
@@ -1,24 +1,41 @@
-from django.shortcuts import render
-from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Listing
 
 
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)
-    paginator = Paginator(listings, 3)
+    paginator = Paginator(listings, 6)
     page = request.GET.get('page')
     paged_listings = paginator.get_page(page)
-    has_other_pages = paged_listings.has_other_pages()
-    has_previous = paged_listings.has_previous()
-    has_next = paged_listings.has_next()
-    page_range = paged_listings.paginator.page_range
-    number = paged_listings.number
+    context = {
+        'listings': paged_listings,
+        'title': 'Browse Our Properties',
+        'description': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt, pariatur!'
+    }
 
-    return render(request, 'listings/listings.html', {'listings': paged_listings})
+    return render(request, 'listings/listings.html', context)
 
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    listing = get_object_or_404(Listing, pk=listing_id)
+    listing_photos = []
+    address = listing.city + ' ' + listing.state + ', ' + listing.zipcode
+
+    for i in range(1, 7):
+        attr = 'photo_' + str(i)
+        photo = getattr(listing, attr)
+        if photo:
+            listing_photos.append(photo)
+
+    context = {
+        'listing': listing,
+        'listing_photos': listing_photos,
+        'title': listing.title,
+        'address': address
+    }
+
+    return render(request, 'listings/listing.html', context)
 
 
 def search(request):
--- pages/views.py
+++ pages/views.py
@@ -1,17 +1,31 @@
 from django.shortcuts import render
+from listings.models import Listing
+from realtors.models import Realtor
 from listings.choices import price_choices, bedroom_choices, state_choices
 
 
 def index(request):
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
 
     context = {
         'price_choices': price_choices,
         'bedroom_choices': bedroom_choices,
-        'state_choices': state_choices
+        'state_choices': state_choices,
+        'listings': listings
     }
 
     return render(request, 'pages/index.html', context)
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+    realtors = Realtor.objects.order_by('-hire_date')
+    mvp_realtor = Realtor.objects.filter(is_mvp=True).first()
+
+    context = {
+        'realtors': realtors,
+        'mvp_realtor': mvp_realtor,
+        'title': 'About BT Real Estate',
+        'description': 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt, pariatur!'
+    }
+
+    return render(request, 'pages/about.html', context)
